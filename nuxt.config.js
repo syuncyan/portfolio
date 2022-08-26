b5df150cbca20d0f40c78e0e8de7950ecdbcdf4d
syuncyan <111932847+syuncyan@users.noreply.github.com>
2022-08-26 17:01:40 +0900

    Initial commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,6 @@
+const title_name = ('Syuncyan portfolio site')
+const desc_name = (自己紹介などいろいろ書いてますぜひ見てね)
+const img_url = url('~assets/portfolio picture.jpg');
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -6,6 +9,12 @@ export default {
   head: {
     title: process.env.npm_package_name || "",
     meta: [
+      { hid: 'description', name: 'description', content: desc_name },
+      { hid: 'og:title', property: 'og:title', content: title_name },
+      { hid: 'og:image', property: 'og:image', content: img_url },
+      { hid: 'og:description', property: 'og:description', content: desc_name },
+      { hid: 'og:url', property: 'og:url', content: syuncyan-portfolio.pages.dev + this.$route.path },
+
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
